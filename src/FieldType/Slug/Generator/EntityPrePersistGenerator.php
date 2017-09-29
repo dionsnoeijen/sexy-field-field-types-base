a38f45fe30b83136c8ff38e6cc813e0d92a2d5a5
Dion Snoeijen <hallo@dionsnoeijen.nl>
2017-09-29 12:51:25 +0200

    Moved out the slug

--- src/FieldType/Slug/Generator/EntityPrePersistGenerator.php
+++ src/FieldType/Slug/Generator/EntityPrePersistGenerator.php
@@ -18,8 +18,8 @@ use Tardigrades\Entity\FieldInterface;
 use Tardigrades\FieldType\Generator\GeneratorInterface;
 use Tardigrades\FieldType\ValueObject\PrePersistTemplate;
 use Tardigrades\FieldType\ValueObject\Template;
+use Tardigrades\SectionField\ValueObject\Slug as SlugValueObject;
 use Tardigrades\SectionField\Generator\Loader\TemplateLoader;
-use Tardigrades\FieldType\Slug\ValueObject\Slug as SlugValueObject;
 
 class EntityPrePersistGenerator implements GeneratorInterface
 {
