45b0b67e34dbff1d86b76800f1d82e0e17bf7113
Paulo <p.moreno@ltp.nl>
2017-11-29 14:34:39 +0100

    FieldType/Email/EmailTest

---
+++ test/unit/FieldType/Email/EmailTest.php
@@ -0,0 +1,59 @@
+<?php
+declare (strict_types=1);
+
+namespace Tardigrades\FieldType\Email;
+
+use PHPUnit\Framework\TestCase;
+use Mockery as M;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Tardigrades\Entity\SectionInterface;
+use Tardigrades\FieldType\FieldType;
+use Tardigrades\SectionField\Service\ReadSectionInterface;
+use Tardigrades\SectionField\Service\SectionManagerInterface;
+use Tardigrades\SectionField\ValueObject\FieldConfig;
+
+/**
+ * @coversDefaultClass Tardigrades\FieldType\Email\Email
+ * @covers ::<private>
+ */
+class EmailTest extends TestCase
+{
+    /**
+     * @test
+     * @covers ::addToForm
+     */
+    public function it_adds_to_form()
+    {
+        $formBuilder = M::mock(FormBuilderInterface::class);
+        $section = M::mock(SectionInterface::class);
+        $sectionEntity = M::mock(FieldType::class);
+        $sectionManager = M::mock(SectionManagerInterface::class);
+        $readSection = M::mock(ReadSectionInterface::class);
+
+        $email = new Email();
+        $config = FieldConfig::fromArray(
+            [
+                'field' =>
+                    [
+                        'name' => 'sexyname',
+                        'handle' => 'lovehandles',
+                        'form' => ['all' => ['emailTesting']]
+                    ]
+            ]
+        );
+        $email->setConfig($config);
+        $sectionEntity->shouldReceive('getId')
+            ->once()
+            ->andReturn(40000);
+
+        $formBuilder->shouldReceive('add')
+            ->once()
+            ->with((string)$email->getConfig()->getHandle(), EmailType::class, ['emailTesting'])
+            ->andReturn($formBuilder);
+        $email->addToForm($formBuilder, $section, $sectionEntity, $sectionManager, $readSection);
+
+        $this->assertInstanceOf(Email::class, $email);
+        $this->assertEquals($email->getConfig(), $config);
+    }
+}
