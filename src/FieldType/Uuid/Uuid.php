19cc7f0589c89736d51f442ab2499a5f2ca44264
Dion Snoeijen <hallo@dionsnoeijen.nl>
2018-03-26 16:33:24 +0200

    Added uuid field base

---
+++ src/FieldType/Uuid/Uuid.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the SexyField package.
+ *
+ * (c) Dion Snoeijen <hallo@dionsnoeijen.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare (strict_types=1);
+
+namespace Tardigrades\FieldType\Uuid;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Tardigrades\Entity\SectionInterface;
+use Tardigrades\FieldType\FieldType;
+use Tardigrades\SectionField\Service\ReadSectionInterface;
+use Tardigrades\SectionField\Service\SectionManagerInterface;
+
+class Uuid extends FieldType
+{
+    public function addToForm(
+        FormBuilderInterface $formBuilder,
+        SectionInterface $section,
+        $sectionEntity,
+        SectionManagerInterface $sectionManager,
+        ReadSectionInterface $readSection
+    ): FormBuilderInterface {
+
+        // If no prePersist, you can add anything to it manually
+        if (!$this->hasEntityEvent('prePersist')) {
+            $options = $this->formOptions($sectionEntity);
+            $formBuilder->add(
+                (string)$this->getConfig()->getHandle(),
+                TextType::class,
+                $options
+            );
+        }
+
+        return $formBuilder;
+    }
+}
