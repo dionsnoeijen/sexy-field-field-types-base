3a08fba5c29d3bfa2298243cadbcdf4e40148135
Paulo <p.moreno@ltp.nl>
2019-02-04 17:03:13 +0100

    new fieldtype for ConfigurationOverride fields

---
+++ src/FieldType/ConfigurationOverride/ConfigurationOverride.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tardigrades\FieldType\ConfigurationOverride;
+
+use Tardigrades\FieldType\Choice\Choice;
+
+class ConfigurationOverride extends Choice
+{
+
+}
