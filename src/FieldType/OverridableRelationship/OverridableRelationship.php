e3d0897de7bc379d360b2d3177e61a030763f2b0
Paulo <p.moreno@ltp.nl>
2019-02-22 13:44:01 +0100

    add OverridableRelationship fieldtype

---
+++ src/FieldType/OverridableRelationship/OverridableRelationship.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tardigrades\FieldType\OverridableRelationship;
+
+use Tardigrades\FieldType\Relationship\Relationship;
+
+class OverridableRelationship extends Relationship
+{
+
+}
